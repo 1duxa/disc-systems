0786c05e8722dbb12c5b29d88ea5e1b8a503f38a
DUXA <andrewvezdel@gmail.com>
2024-02-20 10:35:58 +0200

    Лб2 звіт

--- lb2.js
+++ lb2.js
@@ -2,13 +2,13 @@
 
 //(p ^ q) XOR (p ^ r)  
 
-let p, q, r;
 
 const expression = (a, b, c) => !( ( a&&b ) || ( a&&c ));
+const true_false = x => x ? 'T':'F';
 
 function truth_table (){
 
-    const true_false = x => x ? 'T':'F';
+   
     console.log("| P | Q | R | (P^Q) XOR (P ^ R) |");
     for (let i = 0; i < 2; i++) {
         for (let j = 0; j < 2; j++) {
@@ -26,3 +26,4 @@ function truth_table (){
 
 truth_table();
 
+module.exports = {expression};
---
+++ lb2.test.js
@@ -0,0 +1,5 @@
+const lb2 = require("./lb2");
+
+test("Expression ",()=>{
+    expect(lb2.expression(true,true,true)).toBe(false);
+})
